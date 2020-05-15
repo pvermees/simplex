fd9b355739958a6f4e182e0b02f5f889b3ee1303
Pieter Vermeesch <p.vermeesch@ucl.ac.uk>
2020-05-15 18:41:49 +0100

    calibration function can now plot beta values at any point in time

--- R/calibration.R
+++ R/calibration.R
@@ -1,6 +1,6 @@
-calibration <- function(lr,dc,dat=NULL,x=c('UO2','U238'),
-                                y=c('Pb206','U238'),plot=1,...){
-    B <- beta2york(lr=lr,x=x,y=y)
+calibration <- function(lr,t=0,dc=NULL,dat=NULL,x=c('UO2','U238'),
+                        y=c('Pb206','U238'),plot=1,...){
+    B <- beta2york(lr=lr,t=t,x=x,y=y)
     fit <- IsoplotR:::york(B)
     if (plot>0){
         xlab <- paste0('log[',x[1],'/',x[2],']')
@@ -40,22 +40,42 @@ calibration <- function(lr,dc,dat=NULL,x=c('UO2','U238'),
     invisible(fit)
 }
 
-beta2york <- function(lr,x=c('UO2','U238'),y=c('Pb206','U238')){
+beta2york <- function(lr,t=0,x=c('UO2','U238'),y=c('Pb206','U238')){
     snames <- names(lr)
     ns <- length(snames)
     out <- matrix(0,nrow=ns,ncol=5)
     colnames(out) <- c('X','sX','Y','sY','rXY')
     rownames(out) <- snames
     for (sname in snames){
-        b0g <- lr[[sname]]$b0g
-        covmat <- lr[[sname]]$cov
-        Xlab <- paste0('b0[',x[1],'/',x[2],']')
-        Ylab <- paste0('b0[',y[1],'/',y[2],']')
-        out[sname,'X'] <- b0g[Xlab]
-        out[sname,'Y'] <- b0g[Ylab]
-        out[sname,'sX'] <- sqrt(covmat[Xlab,Xlab])
-        out[sname,'sY'] <- sqrt(covmat[Ylab,Ylab])
-        out[sname,'rXY'] <- covmat[Xlab,Ylab]/(out[sname,'sX']*out[sname,'sY'])
+        b <- b0gt2b(lr=lr[[sname]],t=t,
+                    xlab=paste0(x[1],'/',x[2]),
+                    ylab=paste0(y[1],'/',y[2]))
+        out[sname,'X'] <- b$x[1]
+        out[sname,'Y'] <- b$x[2]
+        out[sname,'sX'] <- sqrt(b$cov[1,1])
+        out[sname,'sY'] <- sqrt(b$cov[2,2])
+        out[sname,'rXY'] <- b$cov[1,2]/(out[sname,'sX']*out[sname,'sY'])
     }
+    out
+}
+
+b0gt2b <- function(lr,t=0,xlab,ylab){
+    b0g <- lr$b0g
+    b0gnames <- names(b0g)
+    nb0g <- length(b0gnames)
+    out <- list()
+    out$x <- rep(0,2)
+    J <- matrix(0,2,nb0g)
+    ibx <- which(b0gnames %in% paste0('b0[',xlab,']'))
+    iby <- which(b0gnames %in% paste0('b0[',ylab,']'))
+    igx <- which(b0gnames %in% paste0('g[',xlab,']'))
+    igy <- which(b0gnames %in% paste0('g[',ylab,']'))
+    out$x[1] <- b0g[ibx] + b0g[igx]*hours(t)
+    out$x[2] <- b0g[iby] + b0g[igy]*hours(t)
+    J[1,ibx] <- 1
+    J[1,igx] <- hours(t)
+    J[2,iby] <- 1
+    J[2,igy] <- hours(t)
+    out$cov <- J %*% lr$cov %*% t(J)
     out
 }
